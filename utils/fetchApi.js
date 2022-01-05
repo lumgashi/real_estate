d0efde132a91e78682fbb4c9bc693be4f9b79421
lumgashi <lumgashi97@hotmail.com>
2022-01-05 22:36:41 +0100

    #fixedSecretKey

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -7,7 +7,7 @@ export const fetchApi = async (url) => {
       const { data } = await axios.get((url), {
              headers: { 
                   'x-rapidapi-host': 'bayut.p.rapidapi.com',
-                  'x-rapidapi-key': '17e5d1a391mshdfd9cc9e6e489dfp1accb8jsn79a4e238cc1e'
+                  'x-rapidapi-key': '#'
              }
       })
       
